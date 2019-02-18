978c10f4af098055a7cfc04afe11bf402691f374
MYZT3RY <d1maz>
2019-02-18 09:25:30 +0300

    reload files

--- intro/source.cpp
+++ intro/source.cpp
@@ -1,16 +1,3 @@
-#include <iostream>
-#include <string.h>
-
-struct meetings{
-	int id;
-	std::string dateOfMeet;
-	std::string timeOfMeet;
-};
-
-meetings meetingsInfo[10];
-
 int main(){
-	FILE * handle;
-	fopen(
 	return false;
 }
