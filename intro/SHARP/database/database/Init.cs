4aaa178862715d14c8438734d5a8e9a1cb092abd
MYZT3RY <d1maz>
2019-04-01 18:27:53 +0300

    transferred code from main program to includable module

---
+++ intro/SHARP/database/database/Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InitClass{
+    class Init{
+        private readonly string equallies = "===========================";
+        private readonly string rigthBar = "|";
+        public void Menu(){
+            Console.Clear();
+            string[] names = { "выбор таблицы", "митинги", "заявители", "нарушения"};
+            Console.WriteLine($"{equallies}");
+            for (int i = 0; i < 4; i++){
+                Console.Write(rigthBar);
+                Console.SetCursorPosition(5, i + 1);
+                Console.Write(names[i]);
+                Console.SetCursorPosition(26, i + 1);
+                Console.WriteLine(rigthBar);
+            }
+            Console.WriteLine($"{equallies}");
+        }
+        public void Sub(){
+            Console.Clear();
+            string[] names = { "что нужно сделать?", "вывести все данные", "добавить данные", "удалить данные", "найти данные", "вернуться обратно" };
+            Console.WriteLine($"{equallies}");
+            for (int i = 0; i < 6; i++){
+                Console.Write(rigthBar);
+                Console.SetCursorPosition(5, i + 1);
+                Console.Write(names[i]);
+                Console.SetCursorPosition(26, i + 1);
+                Console.WriteLine(rigthBar);
+            }
+            Console.WriteLine($"{equallies}");
+        }
+    }
+}
