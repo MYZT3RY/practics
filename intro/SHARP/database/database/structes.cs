07177ccf93a6128470c90806934b0c57c1521071
MYZT3RY <d1maz.d@yandex.ru>
2019-05-14 14:14:50 +0300

    transferred structures from main class

---
+++ intro/SHARP/database/database/structes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Date
+{
+    int day;
+    int month;
+    int year;
+}
+
+public struct Time
+{
+    int hour;
+    int minute;
+}
+
+public struct Meetings
+{
+    public int id;
+    public Date dateOfMeeting;
+    public Time timeOfMeeting;
+    public string addressOfMeeting;
+    public int declaredMembers;
+    public int members;
+    public int listOfMembers;
+    public int acceptFlag;
+}
+
+
+
+struct dName
+{
+    string name;
+    string lastname;
+    string pantronymic;
+}
+
+struct Declared
+{
+    int id;
+    dName name;
+    bool flagOfOffense;
+}
+
+struct Offenses
+{
+    int id;
+    int idOfMeeting;
+    int idOfDeclared;
+    string regulatoryFact;
+    float articleAndParagraphOfRegulatoryFact;
+    bool flagOfCourtConviction;
+}
+
+namespace structes
+{
+    class structes
+    {
+    }
+}
