a22eea67a937954a2a8edfc848d4c8d8823b0352
MYZT3RY <d1maz.d@yandex.ru>
2019-05-29 10:42:57 +0300

    added showBoxWithText & showBox

--- intro/SHARP/database/database/Init.cs
+++ intro/SHARP/database/database/Init.cs
@@ -2,12 +2,13 @@
 
 namespace InitClass{
     class Init{
-        private readonly string equallies = "===========================";
-        private readonly string rigthBar = "|";
+        private const string equall = "=";
+        private const string equellies = "===========================";
+        private const string rigthBar = "|";
         public void Menu(){
             Console.Clear();
-            string[] names = { "выбор таблицы", "митинги", "заявители", "нарушения"};
-            Console.WriteLine($"{equallies}");
+            string[] names = { "выбор таблицы", "митинги", "заявители", "нарушения" };
+            Console.WriteLine(equellies);
             for (int i = 0; i < 4; i++){
                 Console.Write(rigthBar);
                 Console.SetCursorPosition(5, i + 1);
@@ -15,12 +16,12 @@ namespace InitClass{
                 Console.SetCursorPosition(26, i + 1);
                 Console.WriteLine(rigthBar);
             }
-            Console.WriteLine($"{equallies}");
+            Console.WriteLine(equellies);
         }
         public void Sub(){
             Console.Clear();
             string[] names = { "что нужно сделать?", "вывести все данные", "добавить данные", "удалить данные", "найти данные", "вернуться обратно" };
-            Console.WriteLine($"{equallies}");
+            Console.WriteLine(equellies);
             for (int i = 0; i < 6; i++){
                 Console.Write(rigthBar);
                 Console.SetCursorPosition(5, i + 1);
@@ -28,7 +29,40 @@ namespace InitClass{
                 Console.SetCursorPosition(26, i + 1);
                 Console.WriteLine(rigthBar);
             }
-            Console.WriteLine($"{equallies}");
+            Console.WriteLine(equellies);
+        }
+        public void showBoxWithText(int width, int heigth, string message){
+            Console.Clear();
+            for(int x=0; x < width; x++){
+                Console.SetCursorPosition(x, 0);
+                Console.Write(equall);
+                Console.SetCursorPosition(x, heigth-1);
+                Console.Write(equall);
+            }
+            for(int y=0; y < heigth; y++){
+                Console.SetCursorPosition(0, y);
+                Console.Write(rigthBar);
+                Console.SetCursorPosition(width, y);
+                Console.Write(rigthBar);
+            }
+            Console.SetCursorPosition(width / 2 - message.Length / 2, heigth / 2 / 2 + heigth / 2 / 2);
+            Console.Write(message);
+            Console.SetCursorPosition(0, heigth);
+        }
+        public void showBox(int width, int heigth, int headers, int columns){
+            Console.Clear();
+            for (int x = 0; x < width; x++){
+                Console.SetCursorPosition(x, 0);
+                Console.Write(equall);
+                Console.SetCursorPosition(x, heigth - 1);
+                Console.Write(equall);
+            }
+            for(int y=0; y<heigth; y++){
+                Console.SetCursorPosition(0, y);
+                Console.Write(rigthBar);
+                Console.SetCursorPosition(width, y);
+                Console.Write(rigthBar);
+            }
         }
     }
 }
