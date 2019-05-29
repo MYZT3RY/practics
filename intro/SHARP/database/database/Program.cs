d8d46ac519347c8198899966d42933a38fe74d25
MYZT3RY <d1maz.d@yandex.ru>
2019-05-29 10:41:29 +0300

    transferred sctuctes to separate file

--- intro/SHARP/database/database/Program.cs
+++ intro/SHARP/database/database/Program.cs
@@ -1,58 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using MenuClass;
 using FileClass;
-
-struct Date{
-    int day;
-    int month;
-    int year;
-}
-
-struct Time{
-    int hour;
-    int minute;
-}
-
-struct Meetings{
-    int id;
-    Date dateOfMeeting;
-    Time timeOfMeeting;
-    string addressOfMeeting;
-    int declaredMembers;
-    int members;
-    int listOfMembers;
-    int acceptFlag;
-}
-
-struct dName{
-    string name;
-    string lastname;
-    string pantronymic;
-}
-
-struct Declared{
-    int id;
-    dName name;
-    bool flagOfOffense;
-}
-
-struct Offenses{
-    int id;
-    int idOfMeeting;
-    int idOfDeclared;
-    string regulatoryFact;
-    float articleAndParagraphOfRegulatoryFact;
-    bool flagOfCourtConviction;
-}
+using InitClass;
 
 namespace MenuClass{
     class Program{
         static void Main(string[] args){
             var menu = new Menu();
             menu.Main();
-            /*CFile file = new CFile();
-            file.GetConfig("ааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа");
-            Console.Read();*/
         }
     }
 }
